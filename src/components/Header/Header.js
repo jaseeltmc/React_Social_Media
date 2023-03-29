65ac72373955fab15be10196706c06d4de1c0a7a
jaseel <jaseeltmc51@gmail.com>
2023-03-29 12:50:05 +0530

    Home page left section completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,43 +1,45 @@
-import React from 'react'
-import "./Header.css"
-import {Search,Person,Chat,Notifications} from '@mui/icons-material';
+import React from "react";
+import "./Header.css";
+import { Search, Person, Chat, Notifications } from "@mui/icons-material";
 function Header() {
   return (
-    <div className='headerContainer'>
+    <div className="headerContainer">
       <div className="headerLeft">
-        <span className='logo'>JaseelSocial</span>
+        <span className="logo">JaseelSocial</span>
       </div>
       <div className="headerCenter">
         <div className="searchBar">
-            <Search className='searchIcon'/>
-            <input type="text" placeholder='Search for friend,post or video' className="searchInput" />
+          <Search className="searchIcon" />
+          <input
+            type="text"
+            placeholder="Search for friend,post or video"
+            className="searchInput"
+          />
         </div>
       </div>
       <div className="headerRight">
         <div className="headerLinks">
-            <span className="headerLink">Homepage</span>
-            <span className="headerLink">Timeline</span>
-           
+          <span className="headerLink">Homepage</span>
+          <span className="headerLink">Timeline</span>
         </div>
         <div className="headerIcons">
-            <div className="headerIconItem">
-                <Person/>
-                <span className='headerIconBadge'>1</span>
-            </div>
-            <div className="headerIconItem">
-                <Chat/>
-                <span className='headerIconBadge'>1</span>
-            </div>
-            <div className="headerIconItem">
-                <Notifications/>
-                <span className='headerIconBadge'>1</span>
-            </div>
-
+          <div className="headerIconItem">
+            <Person />
+            <span className="headerIconBadge">1</span>
+          </div>
+          <div className="headerIconItem">
+            <Chat />
+            <span className="headerIconBadge">1</span>
+          </div>
+          <div className="headerIconItem">
+            <Notifications />
+            <span className="headerIconBadge">1</span>
+          </div>
         </div>
         <img src="/assets/person/1.jpeg" alt="" className="headerImg" />
       </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,26 @@
+
 import React from 'react'
+import "./Home.css"
 import Header from '../../components/Header/Header'
+import Leftbar from '../../components/Leftbar/Leftbar'
+import Feed from '../../components/Feed/Feed'
+import Rightbar from '../../components/Rightbar/Rightbar'
 
 function home() {
   return (
-    <div>
-    <Header/>
+
+    <>
+     <Header/>
+    <div className="homeContainer">
+    <Leftbar/>
+    <Feed/>
+    <Rightbar/>
     </div>
+    </>
+    
+   
+    
+   
   )
 }
 
